aca4e975b517ef6bf6a5bbfd8dff484cc1c0e784
bilguun0914 <bilguun0914>
2022-05-24 16:04:39 +0800

    up

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -12,7 +12,7 @@ export const lightTheme = {
 export const DarkTheme = {
 
     body:"#000000",
-    text:"#FCF6F4",
+    text:"#FCF6F4", 
     fontFamily:"'Source Sans Pro', sans-serif",
     textRgba : "252, 246, 244",
     bodyRgba:"0,0,0",
