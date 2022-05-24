e995d5992e10868cb2b1aed2a780228cbfc9a877
bilguun0914 <bilguun0914>
2022-05-24 16:27:33 +0800

    up

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -80,7 +80,7 @@ const Intro = () => {
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm Codebucks.</h3>
+                    <h3>I'm Sinnerman.</h3>
                     <h6>I design and Code simple yet beautiful websites.</h6>
                 </Text>
             </SubBox>
